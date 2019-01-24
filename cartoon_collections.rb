348fb48c266efd32439bd2d7483ff04c8a6a677c
Heather Kirk <hskirk@gmail.com>
2019-01-24 17:59:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find {|cheese|}
-  array << cheese_types.include? ("cheddar")
+  cheese_types.include? ("cheddar")
   
 end
